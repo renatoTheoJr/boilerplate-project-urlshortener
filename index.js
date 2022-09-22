08965a27a6781d85d1d512efa29adc73b946e76b
renatoTheoJr <renatotheojunior@gmail.com>
2022-09-22 19:26:24 -0300

    Invalid url

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const {URL} = require('url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 const list = [];
 app.use(cors());
 app.use(express.json())
+const bodyParser = require('body-parser');
+
+const isValidUrl = urlString=> {
+  try { 
+    return Boolean(new URL(urlString)); 
+  }
+  catch(e){ 
+    return false; 
+  }
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -21,17 +36,43 @@ app.get('/api/hello', function(req, res) {
 
 app.post('/api/shorturl', function(request, response){
   const {url} = request.body;
-  const shorturl = list.length + 1;
-  list.push(url);
-  return response.json({
-    original_url: url, 
-    short_url: shorturl
+  const original_url = url;
+  try{
+  const urlObject = new URL(url);
+
+  dns.lookup(urlObject.hostname, (err, address, family) => {
+    if(err){
+      response.json({
+        error: "invalid url"
+      });
+    }else{
+      const shorturl = list.length + 1;
+      const obje =  {
+        original_url,
+        short_url: shorturl
+      }
+      list.push(url);
+      response.json(obje);
+    }
+    
+
   })
+  return response;
+  }catch{
+    return response.json({
+      error: "invalid url"
+    })
+  }
+
+ 
+ 
+ 
 })
 
 app.get('/api/shorturl/:shorturl', function(request, response){
   const {shorturl} = request.params;
-  response.redirect(shorturl);
+  const url = list[shorturl - 1];
+  response.redirect(url);
 })
 
 app.listen(port, function() {
