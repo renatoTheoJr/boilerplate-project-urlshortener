c599c0075fd925680eea5b3f4063a26f761874fa
renatoTheoJr <renatotheojunior@gmail.com>
2022-09-22 17:08:06 -0300

    Challenge

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+const list = [];
 app.use(cors());
-
+app.use(express.json())
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -18,6 +18,21 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', function(request, response){
+  const {url} = request.body;
+  const shorturl = list.length + 1;
+  list.push(url);
+  return response.json({
+    original_url: url, 
+    short_url: shorturl
+  })
+})
+
+app.get('/api/shorturl/:shorturl', function(request, response){
+  const {shorturl} = request.params;
+  response.redirect(shorturl);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
